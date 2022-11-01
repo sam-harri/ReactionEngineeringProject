c896479ef7662b630d265ce00b16d1895c997389
Samuel Harrison <sharr125@uottawa.ca>
2022-11-01 11:25:54 -0400

    Sam 01/11/2022

--- ReactorEquations.py
+++ ReactorEquations.py
@@ -3,12 +3,14 @@ import numpy as np
 from ReactorConstants import ReactorConstants
 from typing import List
 
+FA0: float = ReactorConstants.FA0
+
 class ReactorEquations:
     
     @staticmethod
     def levenspielPlot():
         ydata : List[float] = np.linspace(0,1)
-        xdata : List[float] = [ReactorEquations.calcDissapearence(conversion) for conversion in ydata]
+        xdata : List[float] = [FA0/ReactorEquations.calcDissapearence(conversion) for conversion in ydata]
         plt.plot(ydata,xdata)
         plt.show()
     
