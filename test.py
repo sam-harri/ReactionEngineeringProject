defd4e9bff7dc170130d2b4ecb2c6bc50c313916
Samuel Harrison <sharr125@uottawa.ca>
2022-11-09 15:22:38 -0500

    Sam 09/11/2022

--- test.py
+++ test.py
@@ -7,8 +7,8 @@ import numpy as np
 # xSRP = [873.15, 973.15, 1073.15]
 # ySRP = [7.89, 17.10, 21.70]
 
-x = [873.15, 973.15, 1073.15]
-y = [7.89, 17.10, 21.70]
+x = np.array([873.15, 973.15, 1073.15])
+y = np.array([7.89, 17.10, 21.70])
 
 xKGS = [873.15, 973.15, 1073.15]
 yKGS = [6.45, 13.90, 17.81]
@@ -17,16 +17,13 @@ xRWGS = [873.15, 973.15, 1073.15]
 yRWGS = [5.10, 10.63, 13.65]
 
 poly = PolynomialFeatures(degree=2, include_bias=False)
-# creating a new feature
 poly_features = poly.fit_transform(x.reshape(-1, 1))
-# creating a polynomial regression model
-from sklearn.linear_model import LinearRegression
 poly_reg_model = LinearRegression()
 poly_reg_model.fit(poly_features, y)
 y_predicted = poly_reg_model.predict(poly_features)
+y_test = poly_reg_model.predict(poly.fit_transform(np.linspace(800,1100).reshape(-1, 1)))
 # depicting the polynomial graph
 plt.figure(figsize=(10, 6))
 plt.scatter(x,y)
-plt.plot(x, y_predicted, c='red')
+plt.plot(np.linspace(800,1100), y_test, c='red')
 plt.show()
-
