a4a2690e5c044df5eb671b37a00d217db50bf30a
Samuel Harrison <sharr125@uottawa.ca>
2022-11-16 18:20:17 -0500

    Sam 16/11/2022

--- EnthalpieModelFitting.py
+++ EnthalpieModelFitting.py
@@ -6,13 +6,19 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
+#watch these videos, they helped understand a lot
+#https://www.youtube.com/watch?v=SlXhUjLksgE
+#https://www.youtube.com/watch?v=vgl6a6Y56e8
+#https://www.youtube.com/watch?v=iSsOZ7uw3G8
+
 x_surf, y_surf = np.meshgrid(
     np.linspace(1,10,100),
     np.linspace(600,800,100)
 )
 surfaceX = pd.DataFrame({'pressure' : x_surf.ravel(), 'temperature' : y_surf.ravel()})
 
-df = pd.DataFrame([
+#https://www.osti.gov/biblio/4757372
+df_H2 = pd.DataFrame([
     [1,600,8571.0],
     [1,610,8716.4],
     [1,620,8861.9],
@@ -78,26 +84,62 @@ df = pd.DataFrame([
     [10,800,11499.9]
     ], columns=['pressure', 'temperature', 'enthalpy'])
 
-model = make_pipeline(
+#Elementary Principles of Chemical Processes, 4th Edition
+#Richard M. Felder, Ronald W. Rousseau, Lisa G. Bullard
+df_H2O = pd.DataFrame([
+    [1.01325, 573.15, 55378.11],
+    [1.01325, 623.15, 57215.64],
+    [1.01325, 673.15, 59053.17],
+    [1.01325, 723.15, 60926.73],
+    [1.01325, 773.15, 62836.32],
+    [1.01325, 823.15, 64781.94],
+    [5.06625, 573.15, 55215.97],
+    [5.06625, 623.15, 57071.52],
+    [5.06625, 673.15, 58945.08],
+    [5.06625, 723.15, 60872.68],
+    [5.06625, 773.15, 62764.26],
+    [5.06625, 823.15, 64709.88],
+    [10.12325, 573.15, 54981.78],
+    [10.12325, 623.15, 56909.38],
+    [10.12325, 673.15, 58800.96],
+    [10.12325, 723.15, 60728.56],
+    [10.12325, 773.15, 62656.17],
+    [10.12325, 823.15, 64619.805]
+    ], columns=['pressure', 'temperature', 'enthalpy'])
+
+#H2 Model
+model_H2 = make_pipeline(
     PolynomialFeatures(degree=2),
     LinearRegression()
 )
+model_H2.fit(df_H2[['pressure', 'temperature']], df_H2[['enthalpy']])
+predictedSurface_H2 = model_H2.predict(surfaceX)
 
-model.fit(df[['pressure', 'temperature']], df[['enthalpy']])
+#H2O Model
+model_H2O = make_pipeline(
+    PolynomialFeatures(degree=2),
+    LinearRegression()
+)
+model_H2O.fit(df_H2O[['pressure', 'temperature']], df_H2O[['enthalpy']])
+predictedSurface_H2O = model_H2O.predict(surfaceX)
 
-predictedSurface = model.predict(surfaceX)
 
 figure = plt.figure(figsize=(15,10))
 
 axis = figure.add_subplot(221, projection='3d')
-axis.scatter(df['pressure'],df['temperature'], df['enthalpy'], c='mediumorchid', marker='o', alpha=0.5)
-axis.plot_surface(x_surf, y_surf, predictedSurface.reshape(x_surf.shape), alpha=0.3, color='mediumorchid')
-axis.set_title("Multiple Polynomal Regression Model Hydrogen Enthalpy")
+axis.scatter(df_H2['pressure'],df_H2['temperature'], df_H2['enthalpy'], c='mediumorchid', marker='o', alpha=0.5)
+axis.plot_surface(x_surf, y_surf, predictedSurface_H2.reshape(x_surf.shape), alpha=0.3, color='mediumorchid')
+axis.set_title("Multiple Polynomal Regression Model of H2 Enthalpy")
 axis.set_xlabel("Pressure [atm]")
 axis.set_ylabel("Temperature [K]")
 axis.set_zlabel("Enthalpy [J/g]")
 
 axis2 = figure.add_subplot(222, projection='3d')
-
+axis2.scatter(df_H2O['pressure'],df_H2O['temperature'], df_H2O['enthalpy'], c='royalblue', marker='o', alpha=0.5)
+axis2.plot_surface(x_surf, y_surf, predictedSurface_H2O.reshape(x_surf.shape), alpha=0.3, color='royalblue')
+axis2.set_title("Multiple Polynomal Regression Model of H20 Enthalpy")
+axis2.set_xlabel("Pressure [atm]")
+axis2.set_ylabel("Temperature [K]")
+axis2.set_zlabel("Enthalpy [J/g]")
 
 plt.show()
