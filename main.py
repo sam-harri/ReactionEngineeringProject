f8bef99cab8ef14aa39b8d50ee53a47b418e881f
jbchemE <justin.i.brown@outlook.com>
2022-11-19 23:44:31 -0500

    Justin 19/11/2022

--- main.py
+++ main.py
@@ -7,23 +7,33 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import math
 
+#Lois de vitesses
+r_P:float = -ReactorEquations.r_srp(temp,C_P,C_H2O)
+r_H2O:float = -5*ReactorEquations.r_srp(temp,C_P,C_H2O)-ReactorEquations.r_wgs(temp,C_P,C_CO,C_H2O,C_CO2,C_H2)
+r_H2:float = 8*ReactorEquations.r_srp(temp,C_P,C_H2O)+ReactorEquations.r_wgs(temp,C_P,C_CO,C_H2O,C_CO2,C_H2)
+r_CO:float = 6*ReactorEquations.r_srp(temp,C_P,C_H2O)-ReactorEquations.r_wgs(temp,C_P,C_CO,C_H2O,C_CO2,C_H2)
+r_CO2:float = ReactorEquations.r_wgs(temp,C_P,C_CO,C_H2O,C_CO2,C_H2)
+
+#Débit total
+F_T = F_P+F_H2O+F_H2+F_CO+F_CO2
+
+
 #main method
 if(__name__ == "__main__"):
-    
+    pass
+
     # tempArr = np.linspace(600,800)
     # pressureArr = np.linspace(1,10)
     # feedrateArr = np.linspace(1,10)
     # phenolfractionArr = np.linspace(0,0.1)
     
-    # for T in temp:
-    #     for P in pressure:
-    #         for F in feedrate:
-    #             for PF in phenolfraction:
-    #                 csv.write(method(T,P,F,PF)) #method would return [Volume, Selectivity, ...]
+    # for T in tempArr:
+    #    for P in pressureArr:
+    #       for F in feedrateArr:
+    #            for PF in phenolfractionArr:              
+    #                 csv.write(method(T,P,F,PF)) #method would return [Volume, Selectivity,...]
     
     
     #After, normalize each parameter (value/max for things we want to maximize, min/value for things we want to minimize) such that they are bounded [0,1] and 1 being the best
     #Add all normlized parameters per reactor, reactor setting with highest val is the best overall pick
     #Parameters can also be weighted differently with coefficients in front of normalized value (ex : 2*NormV + 1*NormSelec, places 2x more importance on reactor size)
-    
-    pass
