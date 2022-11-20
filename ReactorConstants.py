04ad708201d0cb4a93cacf3c817ec9a420e204bc
jbchemE <justin.i.brown@outlook.com>
2022-11-19 20:04:29 -0500

    Justin 19/11/2022

--- ReactorConstants.py
+++ ReactorConstants.py
@@ -10,10 +10,9 @@ class ReactorConstants:
     FA0: float = 25 #molH2/s à la sortie
     YI0: float = 0.1 #molI/mol
     U: float = 16.0 #W/m**2K
-    alpha: float = 0 #temp
     weight : float = 0 #temp
     KP: Polynomial = Polynomial("0.000041x^2;-0.0600983x^1;22.88680282250014x^0")
     KH2O: Polynomial = Polynomial("0.0000745x^2;-0.11574935x^1;50.048421226250284x^0")
-    Deq: float = 0.005 #m diamètre équivalent catalyseur
-    epsilon: float = 0.39 #porosité
-    rho: float = 5740 #kg/m^3 masse volumique catalyseur
+    D_eq: float = 0.005 #m diamètre équivalent catalyseur
+    phi: float = 0.39 #porosité
+    rho_c: float = 5740 #kg/m^3 masse volumique catalyseur
