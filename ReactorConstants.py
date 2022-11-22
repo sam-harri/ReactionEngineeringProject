524d84dceeea8eaf6183808b5488e54d13ed277e
jbchemE <justin.i.brown@outlook.com>
2022-11-21 23:12:21 -0500

    Justin 21/11/2022

--- ReactorConstants.py
+++ ReactorConstants.py
@@ -7,10 +7,9 @@ class ReactorConstants:
     #make sure to delcare these as the constants by using the class name everywhere else though
     #ex : FA0: float = ReactorConstants.FA0
     
-    FA0: float = 25 #molH2/s à la sortie
+    F_H2: float = 25 #molH2/s à la sortie
     YI0: float = 0.1 #molI/mol
     U: float = 16.0 #W/m**2K
-    weight : float = 0 #temp
     KP: Polynomial = Polynomial("0.000041x^2;-0.0600983x^1;22.88680282250014x^0")
     KH2O: Polynomial = Polynomial("0.0000745x^2;-0.11574935x^1;50.048421226250284x^0")
     Cp_Ph_model : Polynomial = Polynomial("-0.0000782500x^2;0.242785x^1;68.01049999999961x^0")
@@ -18,7 +17,3 @@ class ReactorConstants:
     phi: float = 0.39 #porosité
     rho_c: float = 5740 #kg/m^3, masse volumique catalyseur
     T_amb: float = 298.15 #K, température ambiante à l'extérieur du réacteur
-
-
-
-    #CAN WE DELETE "weight" AND CHANGE "FA0" TO "F_H2" ?
