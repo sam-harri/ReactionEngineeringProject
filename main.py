72d71aa37997137a91563c8c903057cc33a5b859
Samuel Harrison <sharr125@uottawa.ca>
2022-11-24 21:33:10 -0500

    Sam 24/11/2022

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ import pandas as pd
 #main method
 if(__name__ == "__main__"):
     
-    csvFiles = ["rawReactorData.csv", "processedReactorData.csv", "fullReactorData.csv"]
+    csvFiles = ["CsvData/rawReactorData.csv", "CsvData/processedReactorData.csv", "CsvData/fullReactorData.csv"]
     for csvData in csvFiles:
         tmp = open(csvData, "w")
         tmp.truncate()
@@ -45,29 +45,29 @@ if(__name__ == "__main__"):
     crossSectionAreaRange = np.linspace(0.1, 1, num=5)
     
     # testing the addition of rows to the data
-    # rawDF.loc[len(rawDF.index)] = [0,600,1,30,0.75,0.10,110,12,0.85]
-    # rawDF.loc[len(rawDF.index)] = [1,650,2,33,1.00,0.05,100,11,0.90]
+    rawDF.loc[len(rawDF.index)] = [0,600,1,30,0.75,0.10,110,12,0.85]
+    rawDF.loc[len(rawDF.index)] = [1,650,2,33,1.00,0.05,100,11,0.90]
     
-    run = 0
-    for inletTemperature in temperatureRange:
-        for inletPressure in inletPressureRange:
-            for feedRate in feedRateRange:
-                for phenolFraction in phenolFractionRange:
-                    for crossSectionalArea in crossSectionAreaRange:
-                        volume, selectivity, conversion = ReactorEquations.dimentionlizeReactor(inletTemperature,inletPressure,feedRate,phenolFraction,crossSectionalArea)
-                        rawDF.loc[len(rawDF.index)] = [
-                            run,
-                            inletTemperature,
-                            inletPressure,
-                            feedRate,
-                            phenolFraction,
-                            crossSectionalArea,
-                            volume,
-                            selectivity,
-                            conversion]
-                        run +=1
+    # run = 0
+    # for inletTemperature in temperatureRange:
+    #     for inletPressure in inletPressureRange:
+    #         for feedRate in feedRateRange:
+    #             for phenolFraction in phenolFractionRange:
+    #                 for crossSectionalArea in crossSectionAreaRange:
+    #                     volume, selectivity, conversion = ReactorEquations.dimentionlizeReactor(inletTemperature,inletPressure,feedRate,phenolFraction,crossSectionalArea)
+    #                     rawDF.loc[len(rawDF.index)] = [
+    #                         run,
+    #                         inletTemperature,
+    #                         inletPressure,
+    #                         feedRate,
+    #                         phenolFraction,
+    #                         crossSectionalArea,
+    #                         volume,
+    #                         selectivity,
+    #                         conversion]
+    #                     run +=1
     
-    rawDF.to_csv("rawReactorData.csv", encoding='utf-8', index=False)
+    rawDF.to_csv("CsvData/rawReactorData.csv", encoding='utf-8', index=False)
     
     volMin = rawDF["Volume"].min()
     selMax = rawDF["Selectivity"].max()
@@ -86,10 +86,10 @@ if(__name__ == "__main__"):
         "Normalized Conversion" : normConvArr,
         "Overall Efficiency" : overallRating 
     })
-    normDF.to_csv("processedReactorData.csv", encoding='utf-8', index=False)
+    normDF.to_csv("CsvData/processedReactorData.csv", encoding='utf-8', index=False)
     
     fullDF = pd.merge(rawDF, normDF, on="Run", how="inner")
-    fullDF.to_csv("fullReactorData.csv", encoding="utf-8", index=False)
+    fullDF.to_csv("CsvData/fullReactorData.csv", encoding="utf-8", index=False)
     
     reactorModel = make_pipeline(
         PolynomialFeatures(degree=3),
@@ -105,4 +105,4 @@ if(__name__ == "__main__"):
         fullDF[['Overall Efficiency']])
     
     print(fullDF)
-    #https://stackoverflow.com/questions/54859865/scikit-learn-order-of-coefficients-for-multiple-linear-regression-and-polynomial
+    #https://stackoverflow.com/questions/54859865/scikit-learn-order-of-coefficients-for-multiple-linear-regression-and-polynomia
