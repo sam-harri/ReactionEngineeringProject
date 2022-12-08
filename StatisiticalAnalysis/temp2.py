555382d1ef6439c0cb132a07ce73dd960598c371
Samuel Harrison <sharr125@uottawa.ca>
2022-12-08 13:08:12 -0500

    Sam 12/08/2022

--- StatisiticalAnalysis/temp2.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("CsvData/fullReactorData.csv")
-
-df = df.sort_values(by="Weigth", ascending=True)
-
-df.to_csv("CsvData/bestWeigth.csv", encoding="utf-8", index=False)
